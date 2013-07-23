84169bc2b6203d570148e25b72941a00f174dba3
Randy Eppinger <RandyEppinger@neo.rr.com>
2013-07-23 10:58:40 -0400

    Refactoring.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,14 +1,15 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    controller = ColorsController.alloc.initWithNibName(nil, bundle: nil)
-    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
-    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-    top_nav_controller = create_top_color_nav_controller
-    tab_controller.viewControllers = [nav_controller, top_nav_controller]
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.makeKeyAndVisible
-    @window.rootViewController = tab_controller
+    nav_controller = create_nav_controller
+    top_color_nav_controller = create_top_color_nav_controller
+    tab_controller = create_tab_controller(nav_controller, top_color_nav_controller)
+    create_main_window(tab_controller)
     true
+  end
+
+  def create_nav_controller
+    controller = ColorsController.alloc.initWithNibName(nil, bundle: nil)
+    UINavigationController.alloc.initWithRootViewController(controller)
   end
 
   def create_top_color_nav_controller
@@ -16,4 +17,17 @@ class AppDelegate
     top_controller.title = 'Top Color'
     UINavigationController.alloc.initWithRootViewController(top_controller)
   end
+
+  def create_tab_controller(nav_controller, top_color_nav_controller)
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller, top_color_nav_controller]
+    tab_controller
+  end
+
+  def create_main_window(tab_controller)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+    @window.rootViewController = tab_controller
+  end
+
 end
