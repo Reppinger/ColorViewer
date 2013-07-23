82cd34cd8c7d37d91cf906875ce18b63414f8598
Randy Eppinger <RandyEppinger@neo.rr.com>
2013-07-23 10:52:35 -0400

    Finished 3.3

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,10 +1,19 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.makeKeyAndVisible
     controller = ColorsController.alloc.initWithNibName(nil, bundle: nil)
     nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
-    @window.rootViewController = nav_controller
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    top_nav_controller = create_top_color_nav_controller
+    tab_controller.viewControllers = [nav_controller, top_nav_controller]
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+    @window.rootViewController = tab_controller
     true
+  end
+
+  def create_top_color_nav_controller
+    top_controller = ColorDetailController.alloc.initWithColor(UIColor.purpleColor)
+    top_controller.title = 'Top Color'
+    UINavigationController.alloc.initWithRootViewController(top_controller)
   end
 end
--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -1,31 +1,47 @@
 class ColorsController < UIViewController
+  def initWithNibName(name, bundle:bundle)
+    super
+    self.tabBarItem = UITabBarItem.alloc.initWithTitle('Colors', image: nil, tag: 1)
+    self
+  end
+
   def viewDidLoad
     super
     self.view.backgroundColor = UIColor.whiteColor
-    @label = UILabel.alloc.initWithFrame(CGRectZero)
-    @label.text = 'Colors'
-    @label.sizeToFit
-    @label.center = [self.view.frame.size.width / 2,
-                     self.view.frame.size.height / 2]
-    @label.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
+    @label = create_color_label
     self.view.addSubview(@label)
     self.title = 'Colors'
     ['red', 'green', 'blue'].each_with_index do |color_text, index|
-      color = UIColor.send("#{color_text}Color")
-      button_width = 80
-      button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-      button.setTitle(color_text, forState: UIControlStateNormal)
-      button.setTitleColor(color, forState: UIControlStateNormal)
-      button.sizeToFit
-      button.frame = [
-          [30 + index*(button_width + 10),
-           @label.frame.origin.y + button.frame.size.height + 30],
-          [80, button.frame.size.height]
-      ]
-      button.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
-      button.addTarget(self, action: "tap_#{color_text}", forControlEvents: UIControlEventTouchUpInside)
+      button = create_color_button(color_text, index)
       self.view.addSubview(button)
     end
+  end
+
+  def create_color_label
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+    label.text = 'Colors'
+    label.sizeToFit
+    label.center = [self.view.frame.size.width / 2,
+                     self.view.frame.size.height / 2]
+    label.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
+    label
+  end
+
+  def create_color_button(color_text, index)
+    color = UIColor.send("#{color_text}Color")
+    button_width = 80
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    button.setTitle(color_text, forState: UIControlStateNormal)
+    button.setTitleColor(color, forState: UIControlStateNormal)
+    button.sizeToFit
+    button.frame = [
+        [30 + index*(button_width + 10),
+         @label.frame.origin.y + button.frame.size.height + 30],
+        [80, button.frame.size.height]
+    ]
+    button.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
+    button.addTarget(self, action: "tap_#{color_text}", forControlEvents: UIControlEventTouchUpInside)
+    button
   end
 
   def tap_red
